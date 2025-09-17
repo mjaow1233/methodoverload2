569f126149ff2ae682db0b0eac88e83f80e0f620
Daniel Nilsson <nilsson.daniel1@gmail.com>
2025-09-17 10:57:31 +0200

    Add project files.

---
+++ methodoverload2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace methodoverload2
+{
+    internal class Program
+    {
+        static string FormatName(string first, string last)
+        {
+            return $"{first} {last}";
+        }
+
+        static string FormatName(string first, string middle, string last)
+        {
+            return $"{first} {middle} {last}";
+        }
+
+        static string FormatName(string first, string last, bool uppercase)
+        {
+            var full = $"{first} {last}";
+            return uppercase ? full.ToUpper() : full;
+        }
+    }
+
+    Console.WriteLine(FormatName("Anna", "Svensson"));
+Console.WriteLine(FormatName("Anna", "Maria", "Svensson"));
+Console.WriteLine(FormatName("Anna", "Svensson", true));
+}
